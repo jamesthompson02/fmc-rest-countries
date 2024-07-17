0a0e66c1a9a5425a0100bbb59a92749e7f00b896
jamesthompson02 <jamesthompsongithub@outlook.com>
2024-07-17 15:57:22 +0100

    Have set up the not found page.

--- client/src/app/Pages/not-found-page/not-found-page.component.ts
+++ client/src/app/Pages/not-found-page/not-found-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../Components/button/button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, RouterLink],
   templateUrl: './not-found-page.component.html',
-  styleUrl: './not-found-page.component.scss'
+  styleUrl: './not-found-page.component.scss',
 })
-export class NotFoundPageComponent {
-
-}
+export class NotFoundPageComponent {}
